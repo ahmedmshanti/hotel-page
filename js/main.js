894a9503647c5952b3fcd0de401e22dbd8baa8cc
ahmed <a4medsalem.98@gmail.com>
2023-01-22 15:28:53 +0200

    create hotel page

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let btnBar = document.querySelector(".container button");
+let listItem = document.querySelector(" ul");
+let contentCover = document.querySelector('.content-cover')
+let wow = document.querySelector('.wow')
+btnBar.onclick = function () {
+  listItem.classList.toggle("show");
+};
+
+
+
+
+
